a803145bd3ded77a1368dba0bd97708f516cc9e7
jingyiyi0720-glitch <jingyiyi0720@gmail.com>
2026-06-18 20:03:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const handler = require('./api/screentime');
 
+const PORT = parseInt(process.env.WEB_PORT) || parseInt(process.env.PORT) || 8080;
+
 const server = http.createServer((req, res) => {
   handler(req, res);
 });
 
-server.listen(process.env.WEB_PORT || process.env.PORT || 8080, () => {
-  console.log('Server running on port', process.env.PORT || 3000);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running on port ' + PORT);
 });
