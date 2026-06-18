ce47435c812cbb9c3ef07f66cea5e1c48af59b1d
jingyiyi0720-glitch <jingyiyi0720@gmail.com>
2026-06-18 19:56:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   handler(req, res);
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.WEB_PORT || process.env.PORT || 8080, () => {
   console.log('Server running on port', process.env.PORT || 3000);
 });
